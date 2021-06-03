fa55e6d2864abc018a848a6cb0ab29418113c601
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-06-03 18:53:01 +0300

    /

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './sass/main.scss';
+const KEY = '21922631-b9054864096d193e79c9fc0a3';
+
+fetch(
+  `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=${KEY}`,
+);
